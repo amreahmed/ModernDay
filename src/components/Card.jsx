ba0a05e87dec74345ebdbf3a1c44069b9e12e861
Amr Ezzat <amr.2023001640@buc.edu.eg>
2024-06-28 00:04:24 +0300

    aa

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import { IoIosArrowDropdown, IoIosArrowDropup } from "react-icons/io";
+
 // eslint-disable-next-line react/prop-types
 const Card = ({ title, description, image }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="card bg-[#253042] w-96 shadow-xl flex flex-col justify-between min-h-full">
       <figure>
         <img src={image} alt={title} />
       </figure>
-      <div className="card-body flex-grow">
-        <details className="collapse">
-          <summary className="collapse-title text-xl font-xs text-white">{title}</summary>
-          <div className="collapse-content text-white">
+      <div className="card-body flex-grow ">
+        <details className="collapse" open={isOpen} onToggle={() => setIsOpen((prev) => !prev)}>
+          <summary className="py-2 text-xl font-xs text-white flex items-center cursor-pointer">
+            <div className="flex justify-between items-center  ">
+              <div className="mr-5">{isOpen ? <IoIosArrowDropup className="mr-2" size={20}/> : <IoIosArrowDropdown className="mr-2" size={20}/>}</div>
+              {title}
+            </div>
+          </summary>
+          <div className=" text-white">
             <p>{description}</p>
           </div>
         </details>
